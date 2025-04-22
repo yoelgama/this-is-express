c2a83be17990a3d853f3061c0c7dcd482c478a28
Joel Gama <joeldson.gama.pb@compasso.com.br>
2025-04-22 12:29:48 -0300

    add index.html public page and router to it

--- server/index.js
+++ server/index.js
@@ -8,13 +8,24 @@ DELETE: DELETE RESSOURCE
 
 
 import express from 'express'
+import { dirname } from "path"
+import { fileURLToPath } from 'url'
 const app = express()
 const port = 3000
-
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log()
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
 })
 
 app.get('/', (req, res) => {
-    res.send(req.rawHeaders.toString())
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get('/about', (req, res) => {
+    res.send("Do you want to know about me? Really?")
+})
+
+app.get('/contact', (req, res) => {
+    res.send("So, my number is: 119707070 and, if you can't get it, 70 again.")
 })
