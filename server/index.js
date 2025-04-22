9fbaf2abd2997efbf42d69ec1f800cf5034df91a
Joel Gama <joeldson.gama.pb@compasso.com.br>
2025-04-22 12:04:09 -0300

    added nodemon and GET to server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
+/* 
+GET: REQUEST RESSOURCE
+POST: SENDING RESSOURCE
+PUT: REPLACE RESSOURCE
+PATCH: PATCH UP (SEND A PART OF RESSOURCE) A RESSOURCE
+DELETE: DELETE RESSOURCE
+*/
+
+
 import express from 'express'
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
+})
+
+app.get('/', (req, res) => {
+    res.send(req.rawHeaders.toString())
 })
